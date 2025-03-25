8cbd4592eaeffe85f6aa86a7623f1b67a34aa122
MaxuLinn11 <wiradata26@gmail.com>
2025-03-25 18:22:32 +0700

    Adding Javascript

--- JS/Script.js
+++ JS/Script.js
@@ -1,1 +1,7 @@
 /*Ini Javascript External*/
+function hitungNilaiLuasPersegi() {
+    let menghitungLuasPersegi = document.getElementById("nilai-sisi-luas-persegi").value;
+    let hasilLuasPersegi = parseInt(menghitungLuasPersegi) + parseInt(menghitungLuasPersegi)
+    console.log(hasilLuasPersegi);
+    document.getElementById("result").innerHTML = "Luas persegi tersebut adalah" + " " + hasilLuasPersegi + " " + "m2";
+}
