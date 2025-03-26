aee8472c60ec33b83995df81beb6fbd69b83dd73
MaxuLinn11 <wiradata26@gmail.com>
2025-03-26 11:29:25 +0700

    Add Calculator For All

--- JS/Script.js
+++ JS/Script.js
@@ -3,5 +3,29 @@ function hitungNilaiLuasPersegi() {
     let menghitungLuasPersegi = document.getElementById("nilai-sisi-luas-persegi").value;
     let hasilLuasPersegi = parseInt(menghitungLuasPersegi) + parseInt(menghitungLuasPersegi)
     console.log(hasilLuasPersegi);
-    document.getElementById("result").innerHTML = "Luas persegi tersebut adalah" + " " + hasilLuasPersegi + " " + "m2";
+    document.getElementById("resultLuasPersegi").innerHTML = "Luas persegi tersebut adalah" + " " + hasilLuasPersegi + "m2";
+}
+
+function hitungNilaiKelilingPersegi() {
+    let menghitungKelilingPersegi = document.getElementById("nilai-sisi-keliling-persegi").value;
+    let hasilKelilingPersegi = parseInt(menghitungKelilingPersegi) * (4)
+    console.log(hasilKelilingPersegi);
+      document.getElementById("resultKelilingPersegi").innerHTML = "Keliling persegi tersebut adalah" + " " + hasilKelilingPersegi + "m";
+
+}
+
+function hitungNilaiLuasPersegiPanjang() {
+    let menghitungLuasPanjangPersegiPanjang = document.getElementById("nilai-panjang-luas-persegi-panjang").value;
+    let menghitungLuasLebarPersegiPanjang = document.getElementById("nilai-lebar-luas-persegi-panjang").value;
+    let hasilLuasPersegiPanjang = parseInt(menghitungLuasLebarPersegiPanjang) * parseInt(menghitungLuasPanjangPersegiPanjang);
+    console.log(hasilLuasPersegiPanjang);
+    document.getElementById("resultLuasPersegiPanjang").innerHTML = "Luas persegi panjang tersebut adalah" + " " + hasilLuasPersegiPanjang + "m2";
+}
+
+function hitungNilaiKelilingPersegiPanjang() {
+    let menghitungPanjangKelilingPersegiPanjang = document.getElementById("nilai-panjang-keliling-persegi-panjang").value;
+    let menghitungLebarKelilingPersegiPanjang = document.getElementById("nilai-lebar-keliling-persegi-panjang").value;
+    let hasilKelilingPersegiPanjang = (parseInt(menghitungPanjangKelilingPersegiPanjang) + parseInt (menghitungLebarKelilingPersegiPanjang)) * (2)
+    console.log(hasilKelilingPersegiPanjang);
+        document.getElementById("resultKelilingPersegiPanjang").innerHTML = "Keliling persegi panjang tersebut adalah" + " " + hasilKelilingPersegiPanjang + "m";
 }
